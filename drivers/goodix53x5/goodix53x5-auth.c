663c023c03c01916810b67cc784bc93d9a68c31a
seaweeduk <anthony.hartfield@gmail.com>
2026-07-12 12:07:13 +0200

    Short-circuit single-user identification

--- drivers/goodix53x5/goodix53x5-auth.c
+++ drivers/goodix53x5/goodix53x5-auth.c
@@ -35,6 +35,29 @@ goodix_match_scores_need_exhaustive_logging (void)
   return !g_log_writer_default_would_drop (G_LOG_LEVEL_DEBUG, G_LOG_DOMAIN);
 }
 
+static gboolean
+goodix_gallery_has_single_username (GPtrArray *gallery)
+{
+  const gchar *username;
+
+  if (gallery->len == 0)
+    return FALSE;
+
+  username = fp_print_get_username (g_ptr_array_index (gallery, 0));
+  if (username == NULL || username[0] == '\0')
+    return FALSE;
+
+  for (guint i = 1; i < gallery->len; i++)
+    {
+      FpPrint *print = g_ptr_array_index (gallery, i);
+
+      if (g_strcmp0 (username, fp_print_get_username (print)) != 0)
+        return FALSE;
+    }
+
+  return TRUE;
+}
+
 /* Verify/Identify SSM */
 typedef enum {
   GOODIX_VERIFY_REINIT = 0,
@@ -195,8 +218,12 @@ goodix_verify_ssm_handler (FpiSsm   *ssm,
             int best_match_score = 0;
             int valid_templates = 0;
             gboolean saw_unusable_template = FALSE;
+            gboolean stop_after_match;
 
             fpi_device_get_identify_data (dev, &gallery);
+            stop_after_match =
+              !goodix_match_scores_need_exhaustive_logging () &&
+              goodix_gallery_has_single_username (gallery);
 
             for (guint i = 0; i < gallery->len; i++)
               {
@@ -247,6 +274,13 @@ goodix_verify_ssm_handler (FpiSsm   *ssm,
                           tmpl_best_score = score;
 
                         sample_idx++;
+
+                        if (stop_after_match &&
+                            tmpl_best_score >= GOODIX_SIGFM_BEST_MIN)
+                          {
+                            g_variant_unref (child);
+                            break;
+                          }
                       }
                     g_variant_unref (child);
                   }
@@ -261,6 +295,9 @@ goodix_verify_ssm_handler (FpiSsm   *ssm,
                     best_match_score = tmpl_best_score;
                     match = tmpl;
                   }
+
+                if (stop_after_match && match != NULL)
+                  break;
               }
 
             fp_dbg ("Identify best SIGFM score: %d (best_min: %d)",
